75ef71f1adbc4d954018f2e5f2acfa9fc1b23ff8
pelicanhere <fakegreenall@foxmail.com>
2024-10-05 19:03:35 +0800

    Update Ringfiltration.lean

--- IwasawaAlgebra/Ringfiltration.lean
+++ IwasawaAlgebra/Ringfiltration.lean
@@ -1,1 +1,15 @@
 import Mathlib
+import Mathlib.RingTheory.Ideal.Operations
+import Mathlib.RingTheory.TwoSidedIdeal.Basic
+
+variable {R : Type u} [Ring R]
+
+open Pointwise
+
+/-
+structure RingFiltration {R : Type u} {α : Type v} [Ring R] [CanonicallyOrderedAddCommMonoid α] where
+  F : α → TwoSidedIdeal R
+  bot : F (0 : α) = R
+  intersection_eq  : ∀ i : α, F i = ⨅ j < i, F j
+  inclusion_le : ∀ i j : α,  ((F i) * (F j)) ≤ F (i + j)
+ -/

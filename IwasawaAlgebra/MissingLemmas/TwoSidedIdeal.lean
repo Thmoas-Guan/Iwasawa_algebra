e35a855016e5a60f7443106eaa7b33ca0002a7c5
pelicanhere <fakegreenall@foxmail.com>
2024-10-06 16:24:11 +0800

    Update TwoSidedIdeal.lean

--- IwasawaAlgebra/MissingLemmas/TwoSidedIdeal.lean
+++ IwasawaAlgebra/MissingLemmas/TwoSidedIdeal.lean
@@ -1,33 +1,25 @@
-import Mathlib
-import Mathlib.RingTheory.TwoSidedIdeal.Basic
+import Mathlib.RingTheory.TwoSidedIdeal.operations
 
+variable {R : Type*} [Ring R]
 
-variable {M R : Type*}
-
-namespace test
+namespace TwoSidedIdealextra
 
 open TwoSidedIdeal
 
-variable [Ring R] [AddCommMonoid M] [Module R M] [AddCommMonoid M'] [Module R M'] {I  J : TwoSidedIdeal R}
-
-#check asIdeal (I : TwoSidedIdeal R)
---Here we view TwoSidedIdeal R as a left ideal R
-
-/- def lsmul : R →ₗ[R] M →ₗ[R] M :=
-  mk₂ R (· • ·) add_smul (fun _ _ _ => mul_smul _ _ _) smul_add fun r s m => by
-    simp only [smul_smul, smul_eq_mul, mul_comm]
-
-instance hasSMul' : SMul (Ideal R) (Submodule R M) :=
-  ⟨Submodule.map₂ (LinearMap.lsmul R M)⟩
-
-instance : Mul (Ideal R) :=
-  ⟨(· • ·)⟩
- -/
--- Remark : The above is mulaction about ideal of commRing!!!! So all the definition need to rewrite.
-
-
-def product (I J : TwoSidedIdeal R) : TwoSidedIdeal R := sorry
+/--Define two two-sided ideals product `I * J`
+as the smallest two-sided ideal containing
+all elements of the form `a * b ` for some `a ∈ I, b ∈ J`.-/
+def product (I J : TwoSidedIdeal R) : TwoSidedIdeal R :=
+  TwoSidedIdeal.mk'
+  (carrier := span {x : R | ∃ a ∈ I, ∃ b ∈ J, a * b = x})
+  (ZeroMemClass.zero_mem _)
+  (fun {_ _} h₁ h₂ => AddMemClass.add_mem h₁ h₂)
+  (fun {_} h ↦ NegMemClass.neg_mem h)
+  (fun {x y} h ↦ mul_mem_left _ x y h)
+  (fun {x y} h ↦ mul_mem_right _ x y h)
 
 
 instance : Mul (TwoSidedIdeal R) :=
-  ⟨ product ⟩
+  ⟨product⟩
+
+end TwoSidedIdealextra

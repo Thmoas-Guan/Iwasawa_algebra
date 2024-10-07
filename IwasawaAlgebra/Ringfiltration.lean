96debd0453b4e447b9adb8b2b63b04bd50dc7539
pelicanhere <fakegreenall@foxmail.com>
2024-10-07 10:44:29 +0800

    Update Ringfiltration.lean

--- IwasawaAlgebra/Ringfiltration.lean
+++ IwasawaAlgebra/Ringfiltration.lean
@@ -1,13 +1,16 @@
 import Mathlib.Topology.Algebra.ContinuousMonoidHom
-import Mathlib.Algebra.Category.Ring.Basic
 import Mathlib.CategoryTheory.Category.Basic
 import Mathlib.Order.CompletePartialOrder
 import IwasawaAlgebra.MissingLemmas.TwoSidedIdeal
+import Mathlib.Algebra.Category.Ring.Limits
 
 variable {α : Type v} [CanonicallyOrderedAddCommMonoid α]
+
 section definition
 
-structure RingFiltration (R : Type u) [Ring R] where
+variable (R : Type u) [Ring R]
+
+structure RingFiltration  where
   Fil : α → TwoSidedIdeal R
   bot : Fil (0 : α) = ⊤
   intersection_eq  : ∀ i : α, Fil i = ⨅ j < i, Fil j
@@ -19,10 +22,18 @@ section RingFiltration
 
 variable (R : Type u) [Ring R] (P : RingFiltration R (α := α)) {x : α}
 
-open CategoryTheory Topology
+open CategoryTheory
 
+def Intermap (P : RingFiltration R (α := α)) :=
+  fun (i : α) ↦ (⨅ μ > i, P.Fil μ)
 
-variable {J : Type v} [SmallCategory J] (F : J ⥤ RingCat.{max v u})
+variable {J : Type v} [Category.{w} J] (F : J ⥤ RingCat.{u})
 
-
-def limitConePtAux : Ring (Π j : J, F.obj j) := sorry
+/-- Auxiliary construction to obtain the group structure on the limit of rings. -/
+def limitConePtAux : Subring (Π j : J, F.obj j) where
+  carrier := {x | ∀ ⦃i j : J⦄ (π : i ⟶ j), F.map π (x i) = x j}
+  mul_mem' hx hy _ _ π := by simp_all only [Set.mem_setOf_eq, Pi.mul_apply, map_mul]
+  one_mem' := by simp_all only [Set.mem_setOf_eq, Pi.one_apply, map_one, implies_true]
+  add_mem' := by simp_all only [Set.mem_setOf_eq, Pi.add_apply, map_add, implies_true]
+  zero_mem' := by simp_all only [Set.mem_setOf_eq, Pi.zero_apply, map_zero, implies_true]
+  neg_mem' := by simp_all only [Set.mem_setOf_eq, Pi.neg_apply, map_neg, implies_true]

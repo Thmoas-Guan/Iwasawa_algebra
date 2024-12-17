165428dd081ce1bc880acbd1be700ec2d106cb82
张守信(Shouxin Zhang) <1571193735@qq.com>
2024-12-17 20:52:35 +0800

    Update CompleteGroupRing.lean

--- IwasawaAlgebra/CompleteGroupRing.lean
+++ IwasawaAlgebra/CompleteGroupRing.lean
@@ -7,33 +7,25 @@ import IwasawaAlgebra.MissingLemmas.Limits
 universe u v
 
 open CategoryTheory Topology
-
-
 section lemmas
 
-open Subgroup
+open Subgroup MonoidAlgebra
+#check Profinite
 
-#check MonoidAlgebra
 
 variable {G : Type*} {R : Type*} [Group G] [CommRing R]
 
+def canonical_map {N₁ N₂ : Subgroup G} [N₁.Normal] [N₂.Normal] (h : N₁ ≤ N₂) : G ⧸ N₁ →* G ⧸ N₂ :=
+  QuotientGroup.map (f := MonoidHom.id G) N₁ N₂ h
 
-def caninocal_map {N₁ N₂ : Subgroup G} [N₁.Normal] [N₂.Normal] (h : N₁ ≤ N₂) : G ⧸ N₁ →* G ⧸ N₂ := by
-  refine QuotientGroup.map (f := MonoidHom.id G) N₁ N₂ h
+noncomputable instance test (N : Subgroup G) [N.Normal] : Algebra R (MonoidAlgebra R (G ⧸ N)) := by
+  exact algebra
 
-def canonical_map_GroupRing {N₁ N₂ : Subgroup G} [N₁.Normal] [N₂.Normal] (h : N₁ ≤ N₂) :
-    MonoidAlgebra R (G ⧸ N₁)  →+* MonoidAlgebra R (G ⧸ N₂) := by
-  sorry
-
-  sorry
-
-
-
-
-
-
-
-
+/-- The transition map R[G/N₁] → R[G/N₂] for N₁ ≤ N₂. -/
+noncomputable def transition_map {N₁ N₂ : Subgroup G} [N₁.Normal] [N₂.Normal] (h : N₁ ≤ N₂) :
+  letI := test (R := R) N₂
+  MonoidAlgebra R (G ⧸ N₁) →ₐ[R] MonoidAlgebra R (G ⧸ N₂) := by
+  exact mapDomainAlgHom R R (canonical_map h)
 
 end lemmas
 
